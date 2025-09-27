6a606d8e4b3474b2dde73d5d17e817104ee20389
albertinasampaio <sampaioalbertina078@gmail.com>
2025-09-27 15:24:13 +0100

    meu js

---
+++ asset/js/login.js
@@ -0,0 +1,25 @@
+var nome = " Abertina Sampaio "
+
+function entrar () {
+
+     let idade = 18
+     const cor = " rosa "
+    alert ("Seja bem vindo"+nome +idade +cor);
+
+}
+
+
+
+
+function soma () {
+
+    var soma
+
+    let n1=10;
+    let n2=15;
+
+    soma=n1+n2
+
+    alert ("somatorio de dois numeros"+soma);
+
+}
